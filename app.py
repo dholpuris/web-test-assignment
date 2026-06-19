151a7c45bb4512b792cf6a0bb7966e3362e47a8e
dholpuris <133058732+dholpuris@users.noreply.github.com>
2026-06-19 16:29:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ app = FastAPI()
 @app.get("/")
 def root():
     return{"message": "Hello  Beautiful World"}
+@app.route{"/health"}
+def health():
+    return {"status": "UP"}, 200
+@app.route{"READY"}, 200
